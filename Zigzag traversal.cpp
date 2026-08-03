51254b362be487b58917a2230419ee90878c4a60
Manish saini <sainimk4735@gmail.com>
2026-08-03 17:42:34 +0530

    Create Zigzag traversal.cpp

---
+++ Zigzag traversal.cpp
@@ -0,0 +1,42 @@
+vector<int> zigzagtraversal(Node* root)
+{
+    vector<int>result;
+    if(root==NULL)
+    {
+        return result;
+    }
+
+    queue<Node*>q;
+    bool leftToRight=true;
+    q.push(root);
+
+    while(!q.empty())
+    {
+        int size=q.size();
+        vector<int>ans(size);
+
+        for(int i=0;i<size;i++)
+        {
+            Node* temp=q.front();
+            q.pop();
+
+            int index=leftToRight ? i : size-1-i;
+            ans[index]=temp->data;
+
+            if(temp->left)
+            {
+                q.push(temp->left);
+            }
+            if(temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+        leftToRight!=leftToRight;
+        for(auto i:ans)
+        {
+            result.push_back(i);
+        }
+    }
+    return result;
+}
